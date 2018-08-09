f180585993e4cb64d54501c9475be7bbc14c53bb
Edoardo Z <edovi@icloud.com>
2018-08-09 19:22:23 +0200

    reworks project structure: adds interface module within root module terminalgames, moves renderer in interface module

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 
 extern crate terminalgames;
 
-use terminalgames::renderer::{Renderer};
+use terminalgames::interface::renderer::{Renderer};
 fn main() {
     let a: u32 = 20000;
     let keys: [char; 6] = ['w','a','s','d', '2', 'e'];
