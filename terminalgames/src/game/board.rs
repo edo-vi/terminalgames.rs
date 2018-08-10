5acab82d9ecaf63574c19f9e5bec84ec00440dc7
Edoardo Z <edovi@icloud.com>
2018-08-10 13:45:34 +0200

    adds PartialEq and Debug trait for coordinates; implements getters and setters for Board private field _dimensions

--- terminalgames/src/game/board.rs
+++ terminalgames/src/game/board.rs
@@ -7,22 +7,41 @@ pub enum Tile{
     NonBlocking(Option<char>)
 }
 
-struct Dimensions(u16,u16);
-pub struct Coordinates(u16,u16);
+pub struct Dimensions(pub u16,pub u16);
+
+#[derive(Debug)]
+pub struct Coordinates(pub u16,pub u16);
+
+impl PartialEq for Coordinates{
+    fn eq(&self, other: &Coordinates) -> bool {
+        self.0 == other.0 && self.1 == other.1
+    }
+}
+
 
 ///T is either Vec\<Tile\> or RwLock\<Vec\<Tile\>\>
-pub struct Board<T> {
-    tiles: T,
-    dimensions: Dimensions
+pub struct Board<T>  {
+    _tiles: T,
+    _dimensions: Dimensions
 }
 
 impl<T> Board<T> {
+    pub fn new(tiles: T, dim: Dimensions) -> Board<T> {
+        Board {_tiles: tiles, _dimensions: dim}
+    }
+    pub fn set_dimensions(&mut self, dim: Dimensions) {
+        self._dimensions = dim;
+    }
+
+    pub fn dimensions(&self) -> &Dimensions {
+        &self._dimensions
+    }
     pub fn convert_to_plane(&self, pos: u16) -> Coordinates {
-        Coordinates (pos/self.dimensions.0,pos%self.dimensions.0)
+        Coordinates (pos/(self._dimensions.0+1), pos%(self._dimensions.0+1))
     }
 
     pub fn convert_to_line(&self, pos: Coordinates) -> u16 {
-        pos.0*(self.dimensions.0+1)+pos.1
+        pos.0*(self._dimensions.0+1)+pos.1
     }
 }
 
